5137dfec33f9735445ae8fda9c6646bc191916cd
Gabrqueiroz <gabrqueiroz97@gmail.com>
2026-02-11 19:44:11 -0300

    initial commit

---
+++ src/main/java/com/gabrielqueiroz/event_lifecycle_manager/EventLifecycleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielqueiroz.event_lifecycle_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLifecycleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLifecycleManagerApplication.class, args);
+	}
+
+}
